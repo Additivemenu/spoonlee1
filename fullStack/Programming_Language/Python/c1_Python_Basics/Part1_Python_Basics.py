0407290f4815d999360b753512dba1c1f1767458
additivememu <spoonlee24k@gmail.com>
2022-11-25 08:45:58 +1100

    table-of-content

--- fullStack/Programming_Language/Python/c1_Python_Basics/Part1_Python_Basics.py
+++ fullStack/Programming_Language/Python/c1_Python_Basics/Part1_Python_Basics.py
@@ -3,3 +3,9 @@ message = "happy "+ str(age) + "rd birthday!"
 print(message)
 
 print(message[1])
+
+for num in range(2, 10):  
+    if num % 2 == 0:
+        print("Found an even number", num)
+        pass
+    print("Found a number", num)
--- fullStack/Programming_Language/readme.md
+++ fullStack/Programming_Language/readme.md
@@ -2,3 +2,5 @@
 + [JavaCore1](./javaCore/readme.md)
 + [JavaCore2](./javaCore2/readme.md)
 + [Python](./Python/readme.md)
+
+[鱼皮快乐编程roadmap](https://github.com/liyupi/code-roadmap)
