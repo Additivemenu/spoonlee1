d5b54c27e8535593b667bf8d429340d058506293
XUeshuo Li <spoonlee24k@gmail.com>
2023-05-03 08:06:21 +1000

    Udemy react: 64

--- Udemy/React/C5/class5/src/components/Expenses/ExpenseItem.js
+++ Udemy/React/C5/class5/src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
@@ -6,27 +6,16 @@ import Card from "../UI/Card";
 
 function ExpenseItem(props) {
 
-  // react state
-  const  [title, setTitle] = useState(props.title);
-  console.log("Expense item's state is evaluated");
-
-  const clickHandler = () => {
-    setTitle("Update!");    // not just set value for title, also triggers re-render when title is changed
-    console.log(title);
-  }
-
   return (
     <Card className="expense-item">
 
       <ExpenseDate date={props.date} />
 
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       
-      <button onClick={clickHandler}  >change title</button>  
-
     </Card>
   );
 }
