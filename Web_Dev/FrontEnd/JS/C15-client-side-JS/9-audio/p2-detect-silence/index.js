047d09f49ffa0d8efd14f4e53aaaa8d0e637e13a
XUeshuo Li <spoonlee24k@gmail.com>
2024-08-04 20:36:10 +1000

    added dummy silence detector

--- Web_Dev/FrontEnd/JS/C15-client-side-JS/9-audio/p2-detect-silence/index.js
+++ Web_Dev/FrontEnd/JS/C15-client-side-JS/9-audio/p2-detect-silence/index.js
@@ -5,7 +5,7 @@ const audioPlayback = document.getElementById("audio-playback");
 let mediaRecorder;
 let audioChunks = [];
 let silenceTimeout;
-
+let interval;
 
 startRecordButton.onclick = async () => {
   const stream = await navigator.mediaDevices.getUserMedia({
@@ -13,38 +13,49 @@ startRecordButton.onclick = async () => {
   });
   mediaRecorder = new MediaRecorder(stream);
 
-  // 
+  // setup audio context
   const audioContext = new AudioContext();
   const source = audioContext.createMediaStreamSource(stream);
   const analyser = audioContext.createAnalyser();
-  analyser.fftSize = 2048;
+  // ! config analyser node
+  analyser.fftSize = 512;
   source.connect(analyser);
 
-  const dataArray = new Uint8Array(analyser.fftSize);
+  const dataArray = new Uint8Array(analyser.fftSize); // ! the array that holds the audio data from analyser node
 
   mediaRecorder.ondataavailable = (event) => {
     audioChunks.push(event.data);
   };
 
   mediaRecorder.onstop = () => {
-    const audioBlob = new Blob(audioChunks, { type: "audio/webm" });
+    const audioBlob = new Blob(audioChunks, { type: "audio/webm" }); // chunk data to blob
     const audioUrl = URL.createObjectURL(audioBlob);
     audioPlayback.src = audioUrl;
+
+    startRecordButton.disabled = false;
+    stopRecordButton.disabled = true;
+    clearInterval(interval);
   };
 
   mediaRecorder.start();
   startRecordButton.disabled = true;
   stopRecordButton.disabled = false;
 
-  // ! 
+  // ! callback to tell silence
+  // FIXME: how to check silence continuously?
   const checkSilence = () => {
-    analyser.getByteTimeDomainData(dataArray);  // !  get the time domain data of the audio
+    analyser.getByteTimeDomainData(dataArray); // !  get the time domain data of the audio, FIXME: is this continuous?
     let silent = true;
+
+    console.log("check silence");
+    console.log(dataArray);
+
     for (let i = 0; i < dataArray.length; i++) {
       // 128 is the "no sound" threshold value
       if (dataArray[i] > 128 + 10 || dataArray[i] < 128 - 10) {
         // Adjust threshold as needed
         silent = false;
+
         break;
       }
     }
@@ -52,23 +63,24 @@ startRecordButton.onclick = async () => {
     if (silent) {
       clearTimeout(silenceTimeout);
       silenceTimeout = setTimeout(() => {
-        mediaRecorder.stop();  // ! this stops the recording
-        startRecordButton.disabled = false;
-        stopRecordButton.disabled = true;
+        mediaRecorder.stop(); // ! this stops the recording
       }, 2000); // Stop recording after 2 seconds of silence
     } else {
       clearTimeout(silenceTimeout);
     }
 
-    requestAnimationFrame(checkSilence);
+    // requestAnimationFrame(checkSilence);  // this get run on every frame (16,67ms)
   };
 
-  checkSilence();
+  // check silence in an interval FIXME: how to clear the interval?
+  interval = setInterval(() => {
+    checkSilence();
+  }, 3000);
 };
 
-
 stopRecordButton.onclick = () => {
-  mediaRecorder.stop();  // ! this stops the recording
-  startRecordButton.disabled = false;
-  stopRecordButton.disabled = true;
+  mediaRecorder.stop(); // ! this stops the recording
+  // startRecordButton.disabled = false;
+  // stopRecordButton.disabled = true;
+  // clearInterval(interval);
 };
