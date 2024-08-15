15e552de261dbb355157a329b537b1a212e98a02
XUeshuo Li <spoonlee24k@gmail.com>
2024-08-15 22:48:21 +1000

    added custom handle component to bind node state change

--- Udemy/React/C100-react-libs/02-react-flow/p1-color-compute/src/components/canvas/custom-nodes/ColorPreview.jsx
+++ Udemy/React/C100-react-libs/02-react-flow/p1-color-compute/src/components/canvas/custom-nodes/ColorPreview.jsx
@@ -4,31 +4,14 @@ import {
   useNodesData,
   useHandleConnections,
 } from "@xyflow/react";
+import { useEffect, useState } from "react";
 
+/**
+ * an intermediate node that receives the RGB values and displays the color
+ *
+ */
 function ColorPreview() {
-  const redConnections = useHandleConnections({
-    type: "target",
-    id: "red",
-  });
-  const redNodeData = useNodesData(redConnections?.[0].source);
-
-  const greenConnections = useHandleConnections({
-    type: "target",
-    id: "green",
-  });
-  const greenNodeData = useNodesData(greenConnections?.[0].source);
-
-  const blueConnections = useHandleConnections({
-    type: "target",
-    id: "blue",
-  });
-  const blueNodeData = useNodesData(blueConnections?.[0].source);
-
-  const color = {
-    r: blueNodeData?.data ? redNodeData.data.value : 0,
-    g: greenNodeData?.data ? greenNodeData.data.value : 0,
-    b: blueNodeData?.data ? blueNodeData.data.value : 0,
-  };
+  const [color, setColor] = useState({ r: 0, g: 0, b: 0 });
 
   return (
     <div
@@ -37,41 +20,55 @@ function ColorPreview() {
         background: `rgb(${color.r}, ${color.g}, ${color.b})`,
       }}
     >
-      <div>
-        <Handle
-          type="target"
-          position={Position.Left}
-          id="red"
-          className="handle"
-        />
-        <label htmlFor="red" className="label">
-          R
-        </label>
-      </div>
-      <div>
-        <Handle
-          type="target"
-          position={Position.Left}
-          id="green"
-          className="handle"
-        />
-        <label htmlFor="green" className="label">
-          G
-        </label>
-      </div>
-      <div>
-        <Handle
-          type="target"
-          position={Position.Left}
-          id="blue"
-          className="handle"
-        />
-        <label htmlFor="red" className="label">
-          B
-        </label>
-      </div>
+      {/* for id = red node, if its value gets changed, also update the color state */}
+      <CustomHandle
+        id="red"
+        label="R"
+        onChange={(value) => setColor((c) => ({ ...c, r: value }))}
+      />
+      <CustomHandle
+        id="green"
+        label="G"
+        onChange={(value) => setColor((c) => ({ ...c, g: value }))}
+      />
+      <CustomHandle
+        id="blue"
+        label="B"
+        onChange={(value) => setColor((c) => ({ ...c, b: value }))}
+      />
     </div>
   );
 }
-
 export default ColorPreview;
+
+/**
+ * ! A very good practice:
+ * a custom handle component that isolates connection states and node data binding.
+ * --> react 组件化的思想, 加一个中间层来解耦 (也降低代码重复)
+ */
+function CustomHandle({ id, label, onChange }) {
+  const connections = useHandleConnections({
+    type: "target",
+    id,
+  }); // ! get the connections to this handle
+
+  const nodeData = useNodesData(connections?.[0].source); // ! get the data from the source node
+
+  useEffect(() => {
+    onChange(nodeData?.data ? nodeData.data.value : 0);
+  }, [nodeData]);
+
+  return (
+    <div>
+      <Handle
+        type="target"
+        position={Position.Left}
+        id={id}
+        className="handle"
+      />
+      <label htmlFor="red" className="label">
+        {label}
+      </label>
+    </div>
+  );
+}
--- Udemy/React/C100-react-libs/02-react-flow/p1-color-compute/src/components/canvas/custom-nodes/NumberInput.jsx
+++ Udemy/React/C100-react-libs/02-react-flow/p1-color-compute/src/components/canvas/custom-nodes/NumberInput.jsx
@@ -1,14 +1,21 @@
 import { useCallback, useState } from "react";
 import { Handle, Position, useReactFlow } from "@xyflow/react";
 
+/**
+ * the input node to receive user input for a number as RGB value
+ */
+
 function NumberInput({ id, data }) {
-  const { updateNodeData } = useReactFlow();
+  // we encapsulate this hook in a custom hook in our product 
+  const { updateNodeData } = useReactFlow();  // ! a core hook from react-flow to update the node data
+
   const [number, setNumber] = useState(data.value);
 
   const onChange = useCallback((evt) => {
     const cappedNumber = Math.min(255, Math.max(0, evt.target.value));
-    setNumber(cappedNumber);
-    updateNodeData(id, { value: cappedNumber });
+    setNumber(cappedNumber);    // ! for visual update UI 
+
+    updateNodeData(id, { value: cappedNumber });  // ! for internally updating the data
   }, []);
 
   return (
