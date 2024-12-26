8116881ad60bcf9c222978e90122ad6746fbe240
XUeshuo Li <spoonlee24k@gmail.com>
2024-12-26 22:42:35 +1100

    added a sass demo and some ts notes

--- Web_Dev/FrontEnd/TS/C5-type-programming/06.方括号运算符常见操作/proj/src/index.ts
+++ Web_Dev/FrontEnd/TS/C5-type-programming/06.方括号运算符常见操作/proj/src/index.ts
@@ -1,40 +1,54 @@
 type User = {
-  readonly id: number,
-  name: string
-  tel: string
-  address?: string
-  sex:boolean
+  readonly id: number;
+  name: string;
+  tel: string;
+  address?: string;
+  sex: boolean;
+};
+
+{
+  // [ ]得到类型某个值的类型
+  type DummyValueType = User["id" | "name"]; // type DummyValueType = number | string
+  type ValueType = User[keyof User]; //  type ValueType = string | number | boolean | undefined
 }
 
-// type ValueType = User["id" | "name"];
-type ValueType = User[keyof User]
-
-// 泛型
-type MyReadonly<T> = {
-  readonly [key in keyof T]: T[key];
+{
+  // 泛型
+  type MyReadonly<T> = {
+    readonly [key in keyof T]: T[key];
+  };
 }
 
-// 数组
-const arr = ["admin", "user", "client"] as const;
-const arr2 = [1, true, "admin"] as const;
+{
+  // 数组
+  const arr0 = ["admin", "user", "client"]; // string[]
+  type ArrType0 = (typeof arr0)[number]; // string
 
-// type ArrType = typeof arr[number]
-// type FirstArrType = typeof arr2[0];
-// type LastArrType = typeof arr[2];
+  // 只读元组
+  const arr = ["admin", "user", "client"] as const; // 只读元组
+  const arr2 = [1, true, "admin"] as const;
 
-// 获取元组类型的泛型工具
-type ArrType<T extends readonly any[]> = T[number];
-// type A = ArrType<["admin", "user", "client"]>
-type A = ArrType<typeof arr>
+  type ArrType = (typeof arr)[number]; // "admin" | "user" | "client"
+  type FirstArrType = (typeof arr2)[0]; // 1
+  type LastArrType = (typeof arr)[2]; // "client"
+}
 
-// 获取数组长度 length
-type Len = typeof arr['length'];
-let n: Len = 3;
+{
+  const arr = ["admin", "user", "client"] as const; // as const 得到只读元组
 
-// 获取数组长度的泛型工具
-type ArrLen<T extends readonly any[]> = T['length'];
-type B = ArrLen<[1, 2, 3, 4, 5, 6]>; //6
+  // 获取元组类型的泛型工具
+  type ArrType<T extends readonly any[]> = T[number];
+  // type A = ArrType<["admin", "user", "client"]>
+  type A = ArrType<typeof arr>;   // type A = "admin" | "user" | "client"
 
-// 泛型 + 扩展运算符... + []
-type Concat<T extends any[], U extends any[]> = [...T, ...U]
-type Result = Concat<[1, 2], ["a", "b"]>; // [1,2,"a","b"]
+  // 获取数组长度 length
+  type Len = (typeof arr)["length"];  // type Len = 3, 但如果是arr是number[]的话，Len = number
+  let n: Len = 3;
+  // 获取数组长度的泛型工具
+  type ArrLen<T extends readonly any[]> = T["length"];
+  type B = ArrLen<[1, 2, 3, 4, 5, 6]>; //6
+
+  // 泛型 + 扩展运算符... + []
+  type Concat<T extends any[], U extends any[]> = [...T, ...U]; // ... 对泛型操作? 
+  type Result = Concat<[1, 2], ["a", "b"]>; // [1,2,"a","b"]
+}
--- Web_Dev/FrontEnd/TS/C5-type-programming/07.条件类型与类型兼容性/proj/src/index.ts
+++ Web_Dev/FrontEnd/TS/C5-type-programming/07.条件类型与类型兼容性/proj/src/index.ts
@@ -1,30 +1,41 @@
-type T1 = 1 extends number ? true : false; // true
-type T2 = "1" extends number ? true : false; // false
-type T3 = string extends object ? true : false; // false
-type T4 = string extends Object ? true : false; // true
-type T5 = { a: 1 } extends object ? true : false; // true
-type T6 = { a: 1, b: 2 } extends { a: 1 } ? true : false; // true
-type T7 = { a: 1 } extends { a: 1, b: 2 } ? true : false; // false
-type T8 = string extends {} ? true : false; // true
+{
+  // ! 一定注意条件类型最后取得是类型 (可以是字面量类型), 不是值
+  type T1 = 1 extends number ? true : false; // true
+  type T2 = "1" extends number ? true : false; // false
+  type T3 = string extends object ? true : false; // false, 小写object
+  type T4 = string extends Object ? true : false; // true, 大写的Object
+  type T5 = { a: 1 } extends object ? true : false; // true
+  type T6 = { a: 1; b: 2 } extends { a: 1 } ? true : false; // true
+  type T7 = { a: 1 } extends { a: 1; b: 2 } ? true : false; // false
+  type T8 = string extends {} ? true : false; // true
 
-type T9 = {} extends object ? true : false; // true
-type T10 = object extends {} ? true : false; // true
-type T11 = {} extends Object ? true : false; // true
-type T12 = Object extends {} ? true : false; // true
-type T13 = object extends Object ? true : false; // true
-type T14 = Object extends object ? true : false; // true
+  // 但是，下面的代码会让你产生困惑：
+  // 这三个建议大家不需要细究，知道他们有这个问题：你中有我，我中有你。这是**TS“系统设定”**的问题。
+  // ! 记住这个就行: 原始类型的字面量类型< 原始类型 < 原始类型对应的装箱类型 < Object
+  type T9 = {} extends object ? true : false; // true
+  type T10 = object extends {} ? true : false; // true
 
-// 原始类型的字面量类型< 原始类型 < 原始类型对应的装箱类型 < Object
+  type T11 = {} extends Object ? true : false; // true
+  type T12 = Object extends {} ? true : false; // true
 
-type T15 = string extends any ? true : false; // true
-type T16 = Object extends any ? true : false; // true
-type T17 = Object extends unknown ? true : false; // true
+  type T13 = object extends Object ? true : false; // true
+  type T14 = Object extends object ? true : false; // true
+}
 
-type T18 = any extends Object ? 1 : 2; // 1 | 2
-type T19 = any extends "Hello" ? 1 : 2; // 1 | 2
+{
+  // !记住any/unknown是所有类型的顶层类型就行;
+  type T15 = string extends any ? true : false; // true
+  type T16 = Object extends any ? true : false; // true
+  type T17 = Object extends unknown ? true : false; // true
 
-type T20 = unknown extends any ? 1 : 2; // 1
-type T21 = any extends unknown ? 1 : 2; // 1
+  // 以下神奇 - 但不要纠结, 这是TS的系统设定
+  type T18 = any extends Object ? 1 : 2; // 1 | 2
+  type T19 = any extends "Hello" ? 1 : 2; // 1 | 2
 
-type T22 = never extends "Hello" ? true : false; // true
-type T23 = "Hello" extends never ? true : false; // false
+  type T20 = unknown extends any ? 1 : 2; // 1
+  type T21 = any extends unknown ? 1 : 2; // 1
+
+  // !别忘记，`never`类型是所有类型的子类型
+  type T22 = never extends "Hello" ? true : false; // true
+  type T23 = "Hello" extends never ? true : false; // false
+}
