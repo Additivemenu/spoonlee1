a083da30fd24a59fe20753a9cea8343aef7b97c5
XUeshuo Li <spoonlee24k@gmail.com>
2024-12-31 21:55:37 +1100

    added some TS notes

--- Web_Dev/FrontEnd/TS/C5-type-programming/13.递归复用/proj/src/index.ts
+++ Web_Dev/FrontEnd/TS/C5-type-programming/13.递归复用/proj/src/index.ts
@@ -1,62 +1,72 @@
-// type A = "12345"; // "1" | "2" | "3" | "4" | "5"
+{
+  type A = "12345"; // "1" | "2" | "3" | "4" | "5"
 
-// type StringToUnion<S extends string> = S extends
-//   `${infer One}${infer Two}${infer Three}${infer Four}${infer Five}`
-//   ? One | Two | Three | Four | Five
-//   : never
+  //! 写死的方式
+  // 模板字符串 + infer
+  type StringToUnion<S extends string> =
+    S extends `${infer One}${infer Two}${infer Three}${infer Four}${infer Five}`
+      ? One | Two | Three | Four | Five
+      : never;
+  type B = StringToUnion<A>; // type B = "1" | "2" | "3" | "4" | "5"
 
-// type B = StringToUnion<A>;
+  type NineMantra = "123456789";
+  type C = StringToUnion<NineMantra>; // type C = "1" | "2" | "3" | "4" | "56789"
 
-
-// type NineMantra = "临兵斗者皆阵列前行";
-// type StringToUnion<S extends string> = S extends
-//   `${infer One}${infer Rest}`
-//   ? One | StringToUnion<Rest>
-//   : never
- 
-//   // 临  兵  斗  者  皆   阵  列  前  行
-// type B = StringToUnion<NineMantra>;
-
-
-// 将数组中每个元素类型进行翻转
-
-type A = [1, 2, 3, 4, 5, 6, 7];
-
-type ReverseArr<T extends any[]> = 
-  T extends [infer One, ...infer Rest]
-  ? [...ReverseArr<Rest>, One]
-  : T
-type Revered = ReverseArr<A>;
-
-// 编写类型工具，获取一个字符串字面量类型的长度
-type LengthOfString<S extends string, T extends string[] = []> =
-  S extends `${infer F}${infer R}`
-  ? LengthOfString<R, [...T, F]>
-  : T['length'];
-
-type S = LengthOfString<"12345678">;
-
-// 编写一个类型工具，实现映射类型的深层级的readonly
-type User = {
-  id: number;
-  name: string;
-  address: {
-    province: string;
-    city: {
-      name: string;
-      street: string
-    }
-  }
+  //! 递归实现
+  type StringToUnionRecursion<S extends string> =
+    S extends `${infer One}${infer Rest}`
+      ? One | StringToUnionRecursion<Rest>
+      : never;
+  type D = StringToUnionRecursion<NineMantra>; //  type D = "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"
 }
 
-// type ReadonlyUser = Readonly<User>;
+{
+  // 将数组中每个元素类型进行翻转
+  type A = [1, 2, 3, 4, 5, 6, 7];
 
-type DeepReadonly<T extends Record<string, any>> = 
-  T extends any ?
-  {
-    readonly[K in keyof T]: T[K] extends Record<string, any> ? DeepReadonly<T[K]> : T[K]
-  }
-  : never
+  type ReverseArr<T extends any[]> = T extends [infer One, ...infer Rest]
+    ? [...ReverseArr<Rest>, One]
+    : T;
+  type Revered = ReverseArr<A>; // type Revered = [7, 6, 5, 4, 3, 2, 1]
+}
 
+{
+  //! 上难度
+  // 编写类型工具，获取一个字符串字面量类型的长度
+  type LengthOfString<
+    S extends string,
+    T extends string[] = [],
+  > = S extends `${infer F}${infer R}`
+    ? LengthOfString<R, [...T, F]>
+    : T["length"];
 
-type ReadonlyUser = DeepReadonly<User>;
+  type S = LengthOfString<"12345678">; // type S = 8
+}
+
+{
+  //! 上难度
+  // 编写一个类型工具，实现映射类型的深层级的readonly
+  type User = {
+    id: number;
+    name: string;
+    address: {
+      province: string;
+      city: {
+        name: string;
+        street: string;
+      };
+    };
+  };
+
+  type ReadonlyUser = Readonly<User>; //! only readonly top level
+
+  type DeepReadonly<T extends Record<string, any>> = T extends any
+    ? {
+        readonly [K in keyof T]: T[K] extends Record<string, any>
+          ? DeepReadonly<T[K]>
+          : T[K];
+      }
+    : never;
+
+  type DeepReadonlyUser = DeepReadonly<User>; //! readonly all level
+}
--- Web_Dev/FrontEnd/TS/C5-type-programming/14.分发逆变推断/proj/src/index.ts
+++ Web_Dev/FrontEnd/TS/C5-type-programming/14.分发逆变推断/proj/src/index.ts
@@ -1,31 +1,54 @@
-// 实现类型工具：将联合类型转变为交叉类型
-// 联合类型的分发式特性 + 函数参数的逆变特性 + infer推断
-// 一般情况下，结构化类型遵循的是鸭子类型的协变
-type A = { id: 1, name: "jack" } extends { id: 1 } ? 1 : 2;
-// 函数的参数遵循逆变
-type B = ((arg: { id: 1}) => any) extends
-  ((arg: { id: 1, name: "jack" }) => any) 
-  ? 1
-  : 2
+{
+  // 实现类型工具: 将元组类型转变为交叉类型
+  type TupleToInterSection<T extends any[]> = T extends [infer F, ...infer R]
+    ? F & TupleToInterSection<R>
+    : unknown; // 递归终止条件, 如果写never, any 都不行
 
-type UnionToIntersection<U> =
-  // 利用分发特性，将联合类型构建成联合类型的函数
-  // {id:1} | {name:"jack"} | {sex:"男"}
-  //   (arg: {id:1}) => any
-  // | (arg: {name:"jack"}) => any
-  // | (arg: {sex:"男"}) => any
-  (U extends any ? (arg: U) => any : never) extends (arg: infer P) => any
-  ? P
-  : never
+  type C = TupleToInterSection<
+    [{ id: 1 }, { name: "jack" }, { gender: "male" }]
+  >;
 
+  // what we want:
+  // { id: 1 } & { name: "jack" } & {gender: "male"}
+}
 
-type D = UnionToIntersection<{ id: 1 } | { name: "jack" } | { sex: "男" }>;
+{
+  //! 上难度
+  // 实现类型工具：将联合类型转变为交叉类型
+  // 联合类型的分发式特性 + 函数参数的逆变特性 + infer推断
 
-// 实现类型工具：将元组类型转变为交叉类型
-type TupleToInterSection<T extends any[]> = 
-  T extends [infer F, ...infer R]
-  ? F & TupleToInterSection<R>
-  : unknown
+  // review:
+  // 一般情况下，结构化类型遵循的是鸭子类型的协变
+  type A = { id: 1; name: "jack" } extends { id: 1 } ? 1 : 2; // type A = 1
+  //! 函数的参数遵循逆变
+  type B = ((arg: { id: 1 }) => any) extends (arg: {
+    id: 1;
+    name: "jack";
+  }) => any
+    ? 1
+    : 2; // type B = 1
 
-type C = TupleToInterSection<[{ id: 1 }, { name: "jack" }, { sex: "男" }]>;
-//{ id: 1 } &{ name: "jack" } & { sex: "男" } & unknown
+  // 这是人脑能想出来的?
+  type UnionToIntersection<U> =
+    // 利用分发特性，将联合类型构建成联合类型的函数
+    // {id:1} | {name:"jack"} | {sex:"男"}
+    // 变为
+    //   (arg: {id:1}) => any
+    // | (arg: {name:"jack"}) => any
+    // | (arg: {sex:"男"}) => any
+    (U extends any ? (arg: U) => any : never) extends (arg: infer P) => any
+      ? P
+      : never;
+
+  type D = UnionToIntersection<{ id: 1 } | { name: "jack" } | { sex: "男" }>;
+  /**
+   * 
+   * type D = {
+    id: 1;
+} & {
+    name: "jack";
+} & {
+    sex: "\u7537";
+}
+   */
+}
