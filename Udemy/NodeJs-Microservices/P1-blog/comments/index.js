d82cc1905c438b05626c0f2a7a84aa146ac6e4be
XUeshuo Li <spoonlee24k@gmail.com>
2025-02-22 21:38:57 +1100

    added demo for nodejs microservice until p47

--- Udemy/NodeJs-Microservices/P1-blog/comments/index.js
+++ Udemy/NodeJs-Microservices/P1-blog/comments/index.js
@@ -18,8 +18,9 @@ app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
 
+  // persist comment
   const comments = commentsByPostId[req.params.id] || [];
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: "pending" });
   commentsByPostId[req.params.id] = comments;
 
   // ! send an event to event bus, this will change information in other services
@@ -29,14 +30,44 @@ app.post("/posts/:id/comments", async (req, res) => {
       id: commentId,
       content,
       postId: req.params.id,
+      status: "pending",
     },
   });
 
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   console.log("Received Event", req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === "CommentModerated") {
+    const { postId, id, status, content } = data;
+
+    // ! update the comment status data
+    const comments = commentsByPostId[postId];
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+    comment.status = status;
+
+    commentsByPostId[postId] = comments;
+
+    // emit a new event to the event bus
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        postId,
+        status,
+        content,
+        },
+      })
+      .catch((err) => {
+        console.error(err.message);
+      });
+  }
 
   res.send({});
 });
