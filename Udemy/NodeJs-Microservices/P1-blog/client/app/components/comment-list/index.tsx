cc34685f2dbb997d11633a5a5af9a0c5cfd64b64
XUeshuo Li <spoonlee24k@gmail.com>
2025-02-23 15:04:27 +1100

    added p48 code nodejs microservice

--- Udemy/NodeJs-Microservices/P1-blog/client/app/components/comment-list/index.tsx
+++ Udemy/NodeJs-Microservices/P1-blog/client/app/components/comment-list/index.tsx
@@ -9,11 +9,20 @@ interface CommentListProps {
 export interface Comment {
   id: string;
   content: string;
+  status: "pending" | "approved" | "rejected";
 }
 
 const CommentList = ({ comments }: CommentListProps) => {
   const renderedComments = comments.map((comment) => {
     const c = comment as Comment;
+
+    if (c.status === "pending") {
+      return <li key={c.id}>This comment is awaiting moderation</li>;
+    }
+
+    if (c.status === "rejected") {
+      return <li key={c.id}>This comment has been rejected</li>;
+    }
 
     return (
       <li key={c.id} className="border border-slate-400 p-2 rounded-md">
