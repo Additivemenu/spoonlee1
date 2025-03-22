aa999ffd1b910825b774a032825a3a47328ed7d3
XUeshuo Li <spoonlee24k@gmail.com>
2025-03-23 10:19:39 +1100

    adding threejs camera notes

---
+++ Web_Dev/FrontEnd/threejs-journey/1-basics/p6-camera/camera-orthographic.js
@@ -0,0 +1,76 @@
+// script.js
+
+import * as THREE from "three";
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl-orthographic-camera");
+
+console.log("JavaScript is working");
+
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Camera ==================================================================================================================
+/**
+ * 1st argument: left
+ * 2nd argument: right
+ * 3rd argument: top
+ * 4th argument: bottom
+ * 5th argument: near
+ * 6th argument: far
+ */
+const aspectRatio = sizes.width / sizes.height;
+const camera = new THREE.OrthographicCamera(
+  -1 * aspectRatio, // left - horizontal
+  1 * aspectRatio, // right - horizontal
+  1, // top - vertical
+  -1, // bottom - vertical
+  0.1, // near
+  100, // far
+);
+camera.position.x = 2;
+camera.position.y = 2;
+camera.position.z = 2; // By default, Three.js considers the forward/backward axis to be z. this moves the camera backward
+scene.add(camera);
+
+// Renderer ================================================================================================================
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+/**
+ * !instead of a single render, we use RAF to continuously render the scene with the camera
+ *
+ * option4: Animate - adapt to frame rate using Three.js built-in Clock
+ */
+const clock = new THREE.Clock();
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime(); // return how many seconds have passed since the Clock was created.
+
+  // Update objects
+  mesh.rotation.y = elapsedTime; // ! rotation speed is a constant ((delta of degree / deltaTime)), this is so animation is independent of frame rate on the browser
+
+  // // update camera position
+  // camera.position.x = Math.cos(elapsedTime);
+  // camera.position.y = Math.sin(elapsedTime);
+  camera.lookAt(mesh.position);
+
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame -> chain up the animation
+  window.requestAnimationFrame(tick);
+};
+tick();
---
+++ Web_Dev/FrontEnd/threejs-journey/1-basics/p6-camera/camera-perspective.js
@@ -0,0 +1,72 @@
+// script.js
+
+import * as THREE from "three";
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl-perspective-camera");
+
+console.log("JavaScript is working");
+
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Camera
+
+/**
+ * 1st argument: Field of view (FOV) - how much of the scene is visible, recommended to use 45-75
+ * 2nd argument: aspect ratio (width / height), usually we take the render size
+ * 3rd argument: near clipping plane - the closest point that can be seen
+ * 4th argument: far clipping plane - the farthest point that can be seen
+ */
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100,
+);
+camera.position.x = 2;
+camera.position.y = 2;
+camera.position.z = 2; // By default, Three.js considers the forward/backward axis to be z. this moves the camera backward
+scene.add(camera);
+
+// Renderer ================================================================================================================
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+/**
+ * !instead of a single render, we use RAF to continuously render the scene with the camera
+ *
+ * option4: Animate - adapt to frame rate using Three.js built-in Clock
+ */
+const clock = new THREE.Clock();
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime(); // return how many seconds have passed since the Clock was created.
+
+  // Update objects
+  mesh.rotation.y = elapsedTime; // ! rotation speed is a constant ((delta of degree / deltaTime)), this is so animation is independent of frame rate on the browser
+
+  // update camera position
+  camera.position.x = Math.cos(elapsedTime);
+  camera.position.y = Math.sin(elapsedTime);
+  camera.lookAt(mesh.position);
+
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame -> chain up the animation
+  window.requestAnimationFrame(tick);
+};
+tick();
