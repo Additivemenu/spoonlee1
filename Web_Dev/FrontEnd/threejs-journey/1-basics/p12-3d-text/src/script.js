530883ce2d9fe13c8e8076c9d1e64d22e12416b4
XUeshuo Li <spoonlee24k@gmail.com>
2025-12-25 21:55:14 +1100

    p12: 3D text + 3D donuts

--- Web_Dev/FrontEnd/threejs-journey/1-basics/p12-3d-text/src/script.js
+++ Web_Dev/FrontEnd/threejs-journey/1-basics/p12-3d-text/src/script.js
@@ -1,7 +1,8 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import GUI from "lil-gui";
-import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js"; // three.js built-in font loader
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js"; // three.js built-in text geometry
 
 /**
  * Base
@@ -15,29 +16,82 @@ const canvas = document.querySelector("canvas.webgl");
 // Scene
 const scene = new THREE.Scene();
 
+// axis helper
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
+
 /**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
+const matcapTexture = textureLoader.load("/textures/matcaps/3.png");
+matcapTexture.encoding = THREE.sRGBEncoding;
 
 /**
- * Fonts
+ * load Fonts from local static file
  */
 const fontLoader = new FontLoader();
-fontLoader.load("./fonts/helvetiker_regular.typeface.json", () => {
+fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
   console.log("font loaded");
+  // canvas > scene >  Mesh = Geometry + Material
+  // https://threejs.org/docs/?q=Geometry#TextGeometry
+  const textGeometry = new TextGeometry("Hello Three.js", {
+    font: font,
+    size: 0.5,
+    depth: 0.2,
+    curveSegments: 5,
+    // bevel options, to make the text edges smoother
+    bevelEnabled: true,
+    bevelThickness: 0.03, // in depth direction
+    bevelSize: 0.02, // in size direction
+    bevelOffset: 0,
+    bevelSegments: 5,
+  });
+  textGeometry.center(); // center the geometry
+  //  alternative way to center the text geometry
+  // textGeometry.computeBoundingBox();
+  // // move the text to center (half of its size in each axis negatively)
+  // textGeometry.translate(
+  //   (textGeometry.boundingBox.max.x - 0.02) * -0.5, // bevelSize compensation
+  //   (textGeometry.boundingBox.max.y - 0.02) * -0.5,
+  //   (textGeometry.boundingBox.max.z - 0.03) * -0.5, // bevelThickness compensation
+  // );
+
+  const textMaterial = new THREE.MeshMatcapMaterial({
+    matcap: matcapTexture,
+  });
+  const text = new THREE.Mesh(textGeometry, textMaterial);
+  scene.add(text);
+
+  // create 100 donuts randomly around the text
+  // generate geometry and material first, then reuse them to create multiple meshes to improve performance
+  const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
+  const donutMaterial = new THREE.MeshMatcapMaterial({
+    matcap: matcapTexture,
+  });
+  for (let i = 0; i < 100; i++) {
+    const donut = new THREE.Mesh(donutGeometry, donutMaterial);
+
+    // random position [-5, 5, -5, 5, -5, 5]
+    donut.position.x = (Math.random() - 0.5) * 10;
+    donut.position.y = (Math.random() - 0.5) * 10;
+    donut.position.z = (Math.random() - 0.5) * 10;
+
+    donut.rotation.x = Math.random() * Math.PI;
+    donut.rotation.y = Math.random() * Math.PI;
+
+    const scale = Math.random();
+    donut.scale.set(scale, scale, scale);
+
+    scene.add(donut);
+  }
 });
 
 /**
- * Object
+ * ==============================
+ * window, camera general config
+ * =============================
  */
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial(),
-);
-
-scene.add(cube);
-
 /**
  * Sizes
  */
