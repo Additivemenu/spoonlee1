e04d0c9d822a84fbc82c81eb8b6e0dc8bfd1aa5e
XUeshuo Li <spoonlee24k@gmail.com>
2026-01-18 14:05:09 +1100

    added a simple shell script demo

---
+++ DevOps/Shell/1-custom-cli/config.sh
@@ -0,0 +1,8 @@
+# Configuration file for circle area calculator
+# You can set default values here
+
+# Default radius (can be overridden by command line arguments)
+RADIUS=10
+
+# Default unit (can be overridden by command line arguments)
+UNIT="units"
